63650def00c478c0e085ba1978e56475778cef11
EnisBourbia <enis.bourbia@hotmail.com>
2021-09-27 12:10:05 +0200

    Frontend components skeleton

---
+++ client/src/views/Leaderboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-jumbotron header="leaderboard" lead=""/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'leaderboard',
+  data() {
+    return {
+      message: 'none'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ client/src/views/Quiz.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-jumbotron header="Quiz" lead=""/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'quiz',
+  data() {
+    return {
+      message: 'none'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
